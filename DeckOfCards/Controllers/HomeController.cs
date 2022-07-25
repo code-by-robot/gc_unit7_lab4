3fde260f429b081a1669c1f561973108de099ad9
robot <rahaupt0@gmail.com>
2022-07-25 16:58:30 -0400

    best attempt ft. Josh

---
+++ DeckOfCards/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using DeckOfCards.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DeckOfCards.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public CardsDAL cardsDAL = new CardsDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Draw()
+        {
+            return View();
+        }
+
+        public IActionResult CardsDisplay(int numOfCardsToDraw)
+        {
+            //if(cardsInDeck == 0)
+            //{
+            //    cardsDAL.ShuffleCards();
+            //}
+            CardsModel result1 = cardsDAL.DrawCards(numOfCardsToDraw);
+            return View(result1);
+        }
+
+        [HttpPost]
+        public IActionResult CardsDisplay(List<Card> cards, int numOfCardsToDraw,List<int> keptcards)
+        {
+            //cardsDAL.ShuffleCards();
+            CardsModel result2 = cardsDAL.DrawCards(numOfCardsToDraw-keptcards.Count());
+            List<Card> cardList = new List<Card>();
+            cardList.AddRange(result2.cards);
+            foreach (int number in keptcards)
+            {
+                cardList.Add(cards[number]);
+            }
+            result2.cards = cardList.ToArray();
+            return View(result2);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DeckOfCards/Models/CardsDAL.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace DeckOfCards.Models
+{
+    public class CardsDAL
+    {
+        public CardsModel DrawCards(int numOfCardsToDraw) //adjust here
+        {
+            //sdjust here
+            //api url
+            //string key = "4b91848cccd9013de4ff5c0525b75e91";
+            string url = $"https://deckofcardsapi.com/api/deck/ri6rybcyakh6/draw/?count={numOfCardsToDraw}";
+
+            //setup request
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //save response data
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+
+            //Adjust return
+            //Convert JSON to C# object
+            CardsModel result = JsonConvert.DeserializeObject<CardsModel>(JSON);
+            return result;
+        }
+
+        public void ShuffleCards()
+        {
+            string url = $"https://deckofcardsapi.com/api/deck/ri6rybcyakh6/shuffle/";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        }
+    }
+}
